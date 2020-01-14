4ed3568e03d68130e000254405414968af37001c
Jerome King <2675350965@qq.com>
2020-01-15 01:35:54 +0800

    modify

--- assets/js/includes/header.js
+++ assets/js/includes/header.js
@@ -4,23 +4,11 @@ window.addEventListener("load", function(){
         header.style.height="75px";
     else
         header.style.height="10vh";
-    let words = document.getElementsByClassName("wordchange");
-    let i = 0;
-    while (words[i]){
-        let word = words[i]
-        word.onmouseover=function(){
-            word.style.color='#fff'
-        }
-        word.onmouseleave=function(){
-            word.style.color='#999'
-        }
-        i++;
-    }
     let caidan = document.getElementsByClassName("caidan")[0];
-    words[1].onclick=function(){
+    let moremessage = document.getElementsByClassName("moremessage")[0];
+    moremessage.onclick=function(){
         caidan.style.display=caidan.style.display=="flex"?"none":"flex";
     }
-    let moremessage = this.document.getElementsByClassName("moremessage")[0];
     moremessage.onmouseover=function(){
         this.style.cursor='hand'
     }
