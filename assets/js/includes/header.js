1a8c343f21eb47edffda9c690cd682aeb6844308
Jerome King <2675350965@qq.com>
2020-01-15 14:57:12 +0800

    404 repair

--- assets/js/includes/header.js
+++ assets/js/includes/header.js
@@ -1,9 +1,12 @@
 window.addEventListener("load", function(){
     let header = document.getElementsByClassName("header")[0];
-    if(document.body.clientWidth < document.body.clientHeight )
-        header.style.height="75px";
-    else
-        header.style.height="10vh";
+    let space = document.getElementsByClassName("space")[0];
+    if(document.body.clientWidth < document.body.clientHeight ){
+        header.style.height=space.style.height="75px";
+    }
+    else{
+        header.style.height=space.style.height="10vh";
+    }
     let caidan = document.getElementsByClassName("caidan")[0];
     let moremessage = document.getElementsByClassName("moremessage")[0];
     moremessage.onclick=function(){
