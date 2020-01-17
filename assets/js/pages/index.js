f602e20ab96a374bb90fc53ddceed975afc13c5b
Jerome King <2675350965@qq.com>
2020-01-17 23:44:57 +0800

    modify

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,4 +1,3 @@
-// document.write('<link href="../includes/header.js"></link>')
 window.addEventListener("load", function () {
     let show = document.getElementsByClassName("show")[0];
     let showimage = document.getElementsByClassName("showimage")[0];
@@ -9,42 +8,41 @@ window.addEventListener("load", function () {
     else {
         show.style.height = showcover.style.height = showimage.style.height = "90vh";
     }
-    let bk = new Array();
-    bk[0] = "https://i.loli.net/2020/01/15/WFLbUqanmdS7Q1i.png";
-    bk[1] = "https://i.loli.net/2020/01/15/Id95DUwb8J7HhMn.png";
-    bk[2] = "https://i.loli.net/2020/01/15/Yfx1EX43Lzau57q.png";
-    bk[3] = "https://i.loli.net/2020/01/15/LQD7biJd6FXE2ov.png";
-    bk[4] = "https://i.loli.net/2020/01/15/1qViZK5ov3CXdJs.png";
-    bk[5] = "https://i.loli.net/2020/01/15/bVenAfU7xWRzsuY.png";
-    let changebuttonleft = document.getElementsByClassName("changebutton left")[0];
-    let changebuttonright = document.getElementsByClassName("changebutton right")[0];
-    let i = 0;
-    changebuttonleft.onclick = function () {
-        if (bk[--i])
-            showimage.style.backgroundImage = "url("+bk[i]+")";
-        else {
-            showimage.style.backgroundImage = "url("+bk[bk.length - 1]+")";
-            i = bk.length - 1;
-        }
-    }
-    changebuttonright.onclick = function () {
-        if (bk[++i])
-            showimage.style.backgroundImage = "url("+bk[i]+")";
-        else {
-            showimage.style.backgroundImage = "url("+bk[0]+")";
+    var url = "../../../data/images.json"
+    var request = new XMLHttpRequest();
+    request.open("get", url);
+    request.send(null);
+    request.onload = function () {
+        if (request.status == 200) {
+            let images = JSON.parse(request.responseText);
+            // console.log(images);
+            let changebuttonleft = document.getElementsByClassName("changebutton left")[0];
+            let changebuttonright = document.getElementsByClassName("changebutton right")[0];
             i = 0;
+            changebuttonleft.onclick = function () {
+                i--;
+                if(!images[i])
+                    i=images["length"]-1;
+                showimage.style.backgroundImage = "url(" + images[i]["url"] + ")";
+            }
+            changebuttonright.onclick = function () {
+                i++;
+                if(!images[i])
+                    i=0;
+                showimage.style.backgroundImage = "url(" + images[i]["url"] + ")";
+            }
         }
     }
     const button1 = document.getElementsByTagName('button')[0];
     const button2 = document.getElementsByTagName('button')[1];
     const button3 = document.getElementsByTagName('button')[2];
-    button1.onclick=function(){
-        window.location.href='/emotion'
+    button1.onclick = function () {
+        window.location.href = '/emotion'
     }
-    button2.onclick=function(){
-        window.location.href='/notes'
+    button2.onclick = function () {
+        window.location.href = '/notes'
     }
-    button3.onclick=function(){
-        window.location.href='/share'
+    button3.onclick = function () {
+        window.location.href = '/share'
     }
 });
